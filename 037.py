fb980ec08dddbaf8d8a7687511517af080f87060
Lin <lin.feng.feeling@gmail.com>
2019-08-05 19:23:12 +0100

    Double pointer

---
+++ 037.py
@@ -0,0 +1,35 @@
+class Solution:
+    def threeSum(self, nums):
+
+        if len(nums) < 3:
+            return []
+
+        nums.sort()
+        ans = []
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i]+nums[l]+nums[r]
+                if s == 0:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+
+        return ans
+
+
+nums = [-2, 0, 1, 1, 2]
+rst = Solution().threeSum(nums)
+print(rst)
