c9487a7ba54ea2f4235c55b58623c761c9c5504f
Lin <lin.feng.feeling@gmail.com>
2019-08-11 22:37:33 +0100

    dumy

---
+++ 053.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def increasingBST(self, root):
+        dummy = TreeNode(-1)
+        self.prev = dummy
+        self.inOrder(root)
+        return dummy.right
+
+    def inOrder(self, root):
+        if not root:
+            return None
+        self.inOrder(root.left)
+        root.left = None
+        self.prev.right = root
+        self.prev = self.prev.right
+        self.inOrder(root.right)
